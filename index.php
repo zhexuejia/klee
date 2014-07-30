ec3213f1b15d1ad166ce72b029be6006cfa9162d
Klee <kleetang@gmail.com>
2014-07-30 09:49:05 -0400

    first version

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html class="js no-touch rgba backgroundsize borderradius boxshadow opacity cssanimations csstransitions generatedcontent inlinesvg wf-adobegaramondpro-i4-active wf-adobegaramondpro-i7-active wf-adobegaramondpro-n4-active wf-adobegaramondpro-n7-active wf-brandongrotesque-i5-active wf-brandongrotesque-n3-active wf-brandongrotesque-n4-active wf-brandongrotesque-n5-active wf-brandongrotesque-n7-active wf-active" lang="en"><!--<![endif]-->
+	<head>
+	
+		<title> Klee Tang </title>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<meta name="description" content="This is the portfolio of Klee Tang, a Web Developer/Web Designer from New York.">
+		<meta name="keywords" content="klee, tang, klee tang, web developer, front end developer, web design, graphic design, graphics, illustration, freelance, developer">
+		<meta name="robots" content="index, follow">
+
+		<meta name="msapplication-TileColor" content="#dfe5eb">
+
+		<link rel="icon" type="image/png" href="./img/logo.png">
+
+		<link rel="stylesheet" href="http://seanhalpin.io/assets/css/bootstrap.css">
+
+		<script async="" src="//www.google-analytics.com/analytics.js"></script>
+		<script type="text/javascript" src="//use.typekit.net/wdj6cpr.js"></script>
+		<style type="text/css">.tk-brandon-grotesque{font-family:"brandon-grotesque",sans-serif;}.tk-adobe-garamond-pro{font-family:"adobe-garamond-pro",sans-serif;}</style><link rel="stylesheet" href="http://use.typekit.net/c/60d593/adobe-garamond-pro:i4:i7:n4:n7,brandon-grotesque:i5:n3:n4:n5:n7.Xs1:N:2,Xs0:N:2,Xs2:N:2,Xrz:N:2,Y2p:N:2,Y2j:N:2,Y2l:N:2,Y2n:N:2,Y2q:N:2/d?3bb2a6e53c9684ffdc9a9aff1b5b2a6276adde0ff798449a3bfbdbf8b8f56d4226595a0d2bb6ad9102fa03d20b330fd8deedbb82c099e5f85b8a16b7e0cbe5db66132004fb77917213a9df89796448c617b2698ad1b853f7001748"><script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+		<script src="http://seanhalpin.io/assets/js/respond.min.js"></script>
+		<script src="http://seanhalpin.io/assets/js/modernizr.js"></script>
+
+	</head>
+
+	<body class="">
+		<section id="top">
+			<header id="home-top" class="nav-top nav-fixed nav-root">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4 col-xs-4">
+							<h1 id="header-logo" class="image-text">
+								<a href="/">
+									<span class="mask"></span>
+									<span class="behind"></span>
+									Sean Halpin
+									<i class=""></i>
+								</a>
+							</h1>
+						</div>
+						<div class="col-md-8 col-xs-8">
+							<nav id="top-nav">
+								<ul class="nav-list transistion">
+									<li><a class="active" href="index.php">About</a></li>
+									<li><a href="work.php">Work</a></li>
+									<li><a href="contact.php">Contact</a></li>
+								</ul>
+							</nav>
+						</div> <!-- 12 -->
+					</div> <!-- / row -->
+				</div> <!--/ container -->
+			</header>
+			<div class="masthead-wrap">
+				<div id="title">
+					<div class="container">
+						<div class="transistion cloud left-cloud"></div>
+						<div class="transistion cloud right-cloud"></div>
+						<div class="row">
+							<div class="col-md-12">
+								<div id="head-slider" class="transistion">
+									<div class="slide-contents">
+										<div class="slide">
+											<h2 class="im-sean">
+												<figure class="transistion">
+													<img src="assets/img/content/home/masthead/im-sean.svg">
+												</figure>
+											</h2>
+											<p class="header-desc">a web designer.</p>
+										</div>
+									</div>
+								</div>
+							</div> <!-- /col 24 -->
+						</div> <!-- /row -->
+					</div>
+				</div>
+			</div>
+			<div class="masthead-wrap">
+				<div id="masthead">
+					<div id="earth-holder">
+						<img src="assets/img/content/home/masthead/land.svg">
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="hello">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<article>
+							<figure>
+								<img src="assets/img/content/home/hello/me.svg">
+							</figure>
+							<h3>Hi.</h3>
+							<p>I'm a web designer / developer based in Toronto, Canada. I have a passion for web design and love to create for web and mobile devices.</p>
+						</article>
+					</div>
+				</div>
+			</div>
+		</section>
+		<div class="breaker">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<section id="what-i-do">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<article>
+							<header>
+								<h2>What I can do.</h2>
+							</header>
+							<div id="design" class="row">
+								<div class="col-md-4 col-md-offset-1">
+									<div class="home-design-info">
+										<figure>
+											<img src="assets/img/content/home/hello/me.svg">
+										</figure>
+										<div class="bubble right">
+											<div class="bubbling-design-icons-vert"></div>
+										</div>			
+									</div>
+								</div> <!-- 4 -->
+								<div class="col-md-6">
+									<div class="deets">
+										<h3>Design what you want.</h3>
+										<p>I like to keep it simple. My goals are to focus on typography, content and conveying the message that you want to send.</p>
+									</div>
+								</div>
+							</div> <!-- /row -->
+							<div id="develop" class="row">
+								<div class="col-md-4 col-md-push-7">
+									<div class="home-dev-info">
+										<figure>
+											<img src="assets/img/content/home/what-i-do/me-dev.svg">
+										</figure>
+										<div class="bubble left">
+											<div class="bubbling-dev-icons-vert"></div>
+										</div>			
+									</div>
+								</div> <!-- col -->
+								<div class="col-md-6 col-md-pull-3">
+									<div class="deets">
+										<h3>Develop what you need.</h3>
+										<p>I'm a developer, so I know how to create your website to run across devices using the latest technologies available.</p>
+									</div>
+								</div> <!-- col -->
+							</div> <!-- /row -->
+						</article>
+					</div>
+				</div>
+			</div>
+		</section>
+		<div class="breaker">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<section id="i-can-help">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<article>
+							<header>
+								<h2>I can help.</h2>
+								<h3>I’m currently available for freelance work.</h3>
+							</header>
+							<p>If you have a project that you want to get started, think you need my help with something or just fancy saying hey, then get in touch.</p>
+							<p class="btn-hldr">
+								<a href="mailto:sean.e.halpin@gmail.com" class="main-btn email-lnk">message me <i class="email"></i></a>
+							</p>
+						</article>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="footer-list">
+							<li><a href="http://dribbble.com/seanehalpin">dribbble</a></li>
+							<li><a href="http://instagram.com/seanehalpin">instagram</a></li>
+							<li><a href="https://twitter.com/seanehalpin">twitter</a></li>
+							<li><a href="mailto:sean.e.halpin@gmail.com" class="email-lnk">hire me</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<p>© 2014 <a href="http://seanhalpin.io">Sean Halpin</a>. All rights reserved.</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<script src="./assets/js/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="./assets/js/jquery.min.js"><\/script>')</script>
+		<script src="./assets/js/bootstrap.min.js"></script>
+
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-45121803-1', 'seanhalpin.io');
+		ga('send', 'pageview');
+
+		</script>    
+	</body>
+</html>
